6fc01e0c5feb5eca8ec58c3cb186648ee35ef925
jd1705 <soulbusterjd17@gmail.com>
2025-09-22 14:22:42 -0400

    chore: add MongoDB client setup with collection utility function

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+
+client = MongoClient(mongo_uri)
+db = client["auth_jwt_project"]
+
+def get_collection(collection_name: str):
+    collection = db[collection_name]
+
+    return collection
