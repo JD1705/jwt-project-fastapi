61049e3b6ec3028a4a252f010cd472bdbbfcdc6c
jd1705 <soulbusterjd17@gmail.com>
2025-09-24 18:09:29 -0400

    chore: create user schemas for create, response and database operations

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, field_validator, EmailStr, SecretStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: SecretStr
+    confirm_password: SecretStr
+
+    @field_validator("confirm_password")
+    def validate_passwords(cls, v, values):
+        if "password" in values and v != values["password"]:
+            raise ValueError("Passwords dont match")
+        return v
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+class UserDB(BaseModel):
+    id: str
+    username: str
+    email: EmailStr
+    hashed_password: str
+    created_at: datetime
