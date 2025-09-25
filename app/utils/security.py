f8ead32d9bfcd4f3ef1c4bb317c10313684f52ce
jd1705 <soulbusterjd17@gmail.com>
2025-09-25 14:39:03 -0400

    fix: ensure hashed password is returned as a string

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,11 +1,13 @@
 import bcrypt
 
-def hash_password(password: str) -> bytes:
+def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     bytes_password = password.encode()
 
     hashed_password = bcrypt.hashpw(bytes_password, salt)
-    return hashed_password
+
+    hashed_password_str = hashed_password.decode()
+    return hashed_password_str
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     pass
