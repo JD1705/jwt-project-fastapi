e9e157048e9f99655c67c5197b64055e849f2df7
jd1705 <soulbusterjd17@gmail.com>
2025-10-09 14:15:51 -0400

    chore: implement users/me endpoint in users routes

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.utils.dependencies import get_current_user
+from app.models import UserResponse
 
-router = APIRouter()
+router = APIRouter(prefix="/users")
+
+@router.get("/me", response_model=UserResponse)
+def get_my_profile(current_user: dict = Depends(get_current_user)) -> UserResponse:
+    response = UserResponse(
+            id=str(current_user["_id"]),
+            email=current_user["email"],
+            username=current_user["username"],
+            created_at=current_user["created_at"]
+            )
+
+    return response
